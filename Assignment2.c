b234838f2e87a4facb86bcf2af640cc79cd0b82a
Pranjal <pranjalkuhikar123@gmail.com>
2022-07-15 20:11:08 +0530

    assignment2

---
+++ Assignment2.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    // // 1] Write a program that read a number and print it out.
+    // int a;
+    // printf("Enter the any number :- ");
+    // scanf("%d", &a);
+    // printf("The Number is ",a);
+    // return 0;
+
+    // // 2] Write a program that read a two number and calculate and print the sum  of the number.
+    // int a;
+    // printf("Enter the first number :- ");
+    // scanf("%d", &a);
+    // int b;
+    // printf("Enter the second number :- ");
+    // scanf("%d", &b);
+    // printf("The sum of two number is %d", a + b);
+    // return 0;
+
+    // // 3] Write a program that read a two number and calculate and print the sum  of the number.
+    // int a;
+    // printf("Enter the first number :- ");
+    // scanf("%d", &a);
+    // int b;
+    // printf("Enter the second number :- ");
+    // scanf("%d", &b);
+    // int c;
+    // printf("Enter the third number :- ");
+    // scanf("%d", &c);
+    // printf("The sum of three number is %d\n", a + b + c);
+    // printf("The Average of three number is %d\n", (a + b + c) / 3);
+    // printf("The Multiplication of three number is %d\n", a * b * c);
+    // return 0;
+
+    // // 4] Write a program that read temperature in C that print in F.
+    // int C;
+    // printf("Enter the temperture in C is ");
+    // scanf("%d", &C);
+    // printf("The F tempeture is %d", (C * 9 / 5) + 32);
+    // return 0;
+
+    // // 5]Write a program that read the distance km and print into m,cm,mm.
+    // int distance;
+    // printf("Enter the distance :- ");
+    // scanf("%d", &distance);
+    // printf("km to m = %d\n", distance * 1000);
+    // printf("km to cm = %d\n", distance * 100000);
+    // printf("km to mm = %d\n", distance * 1000000);
+    // return 0;
+
+    // // 6]Write a program that read the weight in mg and convert into g,kg.
+    // float weight;
+    // printf("Enter the weight :- ");
+    // scanf("%f", &weight);
+    // printf("mg to g = %.3f\n", weight / 1000);
+    // printf("mg to kg = %f\n", weight / 1000000);
+    // return 0;
+
+    // // 7]Write a programms that read the value and solve the expressions.
+    int x;
+    printf("Enter the value of  x :- ");
+    scanf("%d", &x);
+    float y;
+    printf("Enter the value of  y :- ");
+    scanf("%f", &y);
+    float r;
+    printf("Enter the value of  r :- ");
+    scanf("%f", &r);
+    int h;
+    printf("Enter the value of  h :- ");
+    scanf("%d", &h);
+    // [a]
+    printf("x+y * x/y = %.1f\n", x + y * x / y);
+    // [b]
+    printf("p = %.1f\n", pow(x, 3) - pow(y, 3));
+    // [c]
+    printf("q = %.1f\n", x * pow(y, 2));
+    // [d]
+    printf("t = %.1f\n", pow(x, 2) - pow(y, 2) + 2 * x * y);
+    // [e]
+    printf("s =  %.1f\n", y * ((pow(x, 2)) - 1));
+    // [f]
+    printf("a = %.1f\n", 2 * 3.24 * r);
+    // [g]
+    printf("z = %.1f", 4 / 3 * 3.14 * r * r * h);
+    return 0;
+}
